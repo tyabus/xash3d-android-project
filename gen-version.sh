330f68c61b6d9f71b479251a35ed81320566ddf2
tyabus <tyabustest@gmail.com>
2021-11-28 19:43:52 +0600

    VersionCode now represents current build

--- gen-version.sh
+++ gen-version.sh
@@ -10,6 +10,7 @@ echo -n '<string name="git_revisions">' >> $GIT_REV_XML
 echo -n '<b>Version information:</b> ' $*_endl| sed -e s/_endl/'\\n'/ >> $GIT_REV_XML
 git submodule --quiet foreach --recursive 'echo -n \<b\>`basename $name`:\</b\>\ `git log --abbrev-commit --pretty=oneline -1` _endl' | sed -e 's/_endl/\\n/g' >> $GIT_REV_XML
 echo -n $USER@$(hostname) $(date +%H:%M:%S-%d-%m-%y) >> $GIT_REV_XML
+sed -i "s/notset/$(printf %04d $(( ( $(date +'%s') - $(date -ud '2015-04-01' +'%s') )/60/60/24 )))/g" AndroidManifest.xml
 echo '</string>' >> $GIT_REV_XML
 echo '</resources>' >> $GIT_REV_XML
 cat $GIT_REV_XML
