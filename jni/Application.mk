204584620e79a1031884270ec196417cf5e77b70
tyabus <tyabustest@gmail.com>
2021-12-02 19:27:31 +0600

    umped TargetSdkVersion to 21 (Android 5.0), Removed hard float abi (dropped in newer ndks), Added 64Bit targets support

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,25 +1,26 @@
 
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-#APP_STL := stlport_static 
+#APP_STL := stlport_static
 
+XASH_64BIT ?= 0
 XASH_SDL ?= 0
-ifeq ($(XASH_SDL),1)
-APP_PLATFORM := android-12
-else
-APP_PLATFORM := android-9
-endif
-
-APP_CFLAGS += -Wl,--no-undefined
-
-# if non-zero, works only if single ABI selected
+# If non-zero, works only if single ABI selected
 XASH_THREAD_NUM ?= 0
 
+#NDK_TOOLCHAIN_VERSION := clang
+
+ifneq ($(NDK_TOOLCHAIN_VERSION),clang)
+APP_CFLAGS += -Wl,--no-undefined
+else
+APP_CFLAGS +=
+endif
 
 CFLAGS_OPT :=  -O3 -fomit-frame-pointer -ggdb -funsafe-math-optimizations -ftree-vectorize -fgraphite-identity -floop-interchange -funsafe-loop-optimizations -finline-limit=256 -pipe
 CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a9 -pipe -mvectorize-with-neon-quad -DVECTORIZE_SINCOS -fPIC -DHAVE_EFFICIENT_UNALIGNED_ACCESS
+CFLAGS_OPT_ARM64 := -pipe
+CFLAGS_OPT_X86_64 := -pipe -funroll-loops
 CFLAGS_OPT_X86 := -mtune=atom -march=atom -mssse3 -mfpmath=sse -funroll-loops -pipe -DVECTORIZE_SINCOS -DHAVE_EFFICIENT_UNALIGNED_ACCESS
-CFLAGS_HARDFP := -D_NDK_MATH_NO_SOFTFP=1 -mhard-float -mfloat-abi=hard -DLOAD_HARDFP -DSOFTFP_LINK
 APPLICATIONMK_PATH = $(call my-dir)
 
 NANOGL_PATH := $(APPLICATIONMK_PATH)/src/NanoGL/nanogl
@@ -30,11 +31,13 @@ HLSDK_PATH  := $(APPLICATIONMK_PATH)/src/HLSDK/halflife/
 
 XASH3D_CONFIG := $(APPLICATIONMK_PATH)/xash3d_config.mk
 
-APP_ABI := x86 armeabi-v7a-hard
-# Use armeabi-v7a to disable hardfloat, armeabi to build armv5 xash3d
-# Change CFLAGS_OPT_ARMv5 to "-mfloat-abi=softfp -mfpu=vfp" and fix setup.mk of ndk to build armv6
+ifeq ($(XASH_64BIT),1)
+APP_ABI := x86_64 arm64-v8a
+else
+APP_ABI := x86 armeabi-v7a
+endif
+# Use armeabi-v7a-hard to enable hardfloat (r12b and higher ndks dosent support hard float)
 # Mods are built with both ABI support
-# ARMv6 and ARMv5 xash3d builds use softfp only and compatible only with softfp mods
 # Build both armeabi-v7a-hard and armeabi-v7a supported only for mods, not for engine
 
 APP_MODULES := xash menu client server NanoGL
--- jni/xash3d_config.mk
+++ jni/xash3d_config.mk
@@ -1,13 +1,16 @@
-LOCAL_CFLAGS += $(CFLAGS_OPT)
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a-hard)
-LOCAL_CFLAGS += $(CFLAGS_OPT_ARM) $(CFLAGS_HARDFP)
-endif
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 LOCAL_CFLAGS += $(CFLAGS_OPT_ARM) -mfloat-abi=softfp
+endif
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+LOCAL_CFLAGS += $(CFLAGS_OPT_ARM64)
 endif
 ifeq ($(TARGET_ARCH_ABI),x86)
 LOCAL_CFLAGS += $(CFLAGS_OPT_X86)
 endif
+ifeq ($(TARGET_ARCH_ABI),x86_64)
+LOCAL_CFLAGS += $(CFLAGS_OPT_X86_64)
+endif
+
 ifneq ($(XASH_THREAD_NUM),0)
 ifneq (,$(findstring $(LOCAL_MODULE),nanogl server client))
 #LOCAL_LDFLAGS += -fopenmp -ftree-parallelize-loops=$(XASH_THREAD_NUM)
