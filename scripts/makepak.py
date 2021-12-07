e356b2795db917f6df6228ed7f42c8a39954af03
tyabus <tyabustest@gmail.com>
2021-12-08 00:04:01 +0600

    "Port" makepak.py to python3, added failsafe checks

--- scripts/makepak.py
+++ scripts/makepak.py
@@ -1,21 +1,31 @@
+#!/usr/bin/env python3
+
 import sys
 import struct
 import os
 
-#dummy class for stuffing the file headers into
+# Dummy class for stuffing the file headers into
 class FileEntry:
     pass
 
-#arguments are source directory, then target filename e.g. "pak1.pak"
-rootdir = sys.argv[1]
-pakfilename = sys.argv[2]
+# Arguments are source directory, then target filename e.g. "pak0.pak"
+if len(sys.argv) == 3: # Check if arguments are even specified
+    rootdir = sys.argv[1]
+    pakfilename = sys.argv[2]
+else:
+    print(sys.argv[0]+": <root dir> <pakfilename>")
+    sys.exit(0)
 
-pakfile = open(pakfilename,"wb")
+try:
+    pakfile = open(pakfilename,"wb")
+except IOError:
+    print(pakfilename+": IO Error")
+    sys.exit(1)
 
-#write a dummy header to start with
+# Write a dummy header to start with
 pakfile.write(struct.Struct("<4s2l").pack(b"PACK",0,0))
 
-#walk the directory recursively, add the files and record the file entries
+# Walk the directory recursively, add the files and record the file entries
 offset = 12
 fileentries = []
 for root, subFolders, files in os.walk(rootdir):
@@ -23,8 +33,8 @@ for root, subFolders, files in os.walk(rootdir):
         entry = FileEntry()
         impfilename = os.path.join(root,file)
         entry.filename = os.path.relpath(impfilename,rootdir).replace("\\","/")
-        if(entry.filename.startswith(".git")):continue
-	print "pak: "+entry.filename
+        if(entry.filename.startswith(".git")): continue
+        print("pak: "+entry.filename)
         with open(impfilename, "rb") as importfile:
             pakfile.write(importfile.read())
             entry.offset = offset
@@ -33,13 +43,13 @@ for root, subFolders, files in os.walk(rootdir):
         fileentries.append(entry)
 tablesize = 0
 
-#after all the file data, write the list of entries
+# After all the file data, write the list of entries
 for entry in fileentries:
     pakfile.write(struct.Struct("<56s").pack(entry.filename.encode("ascii")))
     pakfile.write(struct.Struct("<l").pack(entry.offset))
     pakfile.write(struct.Struct("<l").pack(entry.length))
     tablesize = tablesize + 64
 
-#return to the header and write the values correctly
+# Return to the header and write the values correctly
 pakfile.seek(0)
 pakfile.write(struct.Struct("<4s2l").pack(b"PACK",offset,tablesize))
