4833d7c3900d6408e67ae7c7fd78f5722d5d0ea8
Mr0maks <mr.maks0443@gmail.com>
2023-06-24 17:48:49 +0500

    Tune code for Cortex A53

--- jni/Application.mk
+++ jni/Application.mk
@@ -15,10 +15,10 @@ APP_PLATFORM := android-15
 #NDK_TOOLCHAIN_VERSION := clang
 
 CFLAGS_OPT :=  -O3 -fomit-frame-pointer -ggdb -funsafe-math-optimizations -ftree-vectorize -fgraphite-identity -floop-interchange -funsafe-loop-optimizations -finline-limit=256 -pipe
-CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a9 -pipe -DVECTORIZE_SINCOS -fPIC -DHAVE_EFFICIENT_UNALIGNED_ACCESS
-CFLAGS_OPT_ARM64 := -pipe
-CFLAGS_OPT_X86_64 := -pipe -funroll-loops
-CFLAGS_OPT_X86 := -mtune=atom -march=atom -mssse3 -mfpmath=sse -funroll-loops -pipe -DVECTORIZE_SINCOS -DHAVE_EFFICIENT_UNALIGNED_ACCESS
+CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a53 -DVECTORIZE_SINCOS -fPIC -DHAVE_EFFICIENT_UNALIGNED_ACCESS
+CFLAGS_OPT_ARM64 := -mcpu=cortex-a53 -DVECTORIZE_SINCOS
+CFLAGS_OPT_X86_64 := -funroll-loops
+CFLAGS_OPT_X86 := -mtune=atom -march=atom -mssse3 -mfpmath=sse -funroll-loops -DVECTORIZE_SINCOS -DHAVE_EFFICIENT_UNALIGNED_ACCESS
 CFLAGS_HARDFP := -D_NDK_MATH_NO_SOFTFP=1 -mhard-float -mfloat-abi=hard -DLOAD_HARDFP -DSOFTFP_LINK
 APPLICATIONMK_PATH = $(call my-dir)
 
